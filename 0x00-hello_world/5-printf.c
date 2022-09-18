825f8f3ef3fb66693bd9238b82d15fe4d7f8f453
CyrilOputa <cyriloputa@gmail.com>
2022-09-18 15:10:22 -0700

    Still working with C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 is a pass
+*/
+
+int main(void)/*defining the main function as an integer with no arguments*/
+{
+	printf("with proper grammar, but thr outcome is a piece of art,\n");
+	return (0);
+
+}
