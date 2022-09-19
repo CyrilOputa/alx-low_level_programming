543616c5b98ed1332a4d5d4c7de8bb204ad6d5c3
CyrilOputa <cyriloputa@gmail.com>
2022-09-19 12:47:00 -0700

    How do I stay on top?

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)/*defining the main function as an integer with no arguments*/
 {
-	printf("with proper grammar, but thr outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
